6d280c5a4abfd97dae434dadeb47baae1cd293a5
timbaron <timothy.akiode@student.aul.edu.ng>
2022-07-28 06:59:46 +0100

    Display article template

--- server.js
+++ server.js
@@ -1,9 +1,24 @@
 const express = require('express');
 const app = express();
+const articleRouter = require('./routes/article');
 // set up view engine
 app.set('view engine', 'ejs');
-
+app.use('/articles',articleRouter);
 app.get('/', (req, res) => {
-    res.render('index');
+    const articles = [
+        {
+            id: 1,
+            title: 'Article 1',
+            createdAt: Date.now(),
+            description: 'Article description This is article 1'
+        },
+        {
+            id: 2,
+            title: 'Article 2',
+            createdAt: Date.now(),
+            description: 'Article description This is article 2'
+        },
+    ];
+    res.render('index', {articles: articles});
 })
 app.listen(5000)
